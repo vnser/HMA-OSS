6628ab6cf407f0b7a07f5f89d518ca2125a554a1
frknkrc44 <krc440002@gmail.com>
2025-08-18 04:14:52 +0300

    Add MIUI fix (WIP)

--- xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/XposedEntry.kt
+++ xposed/src/main/java/icu/nullptr/hidemyapplist/xposed/XposedEntry.kt
@@ -1,5 +1,6 @@
 package icu.nullptr.hidemyapplist.xposed
 
+import android.content.Intent
 import android.content.pm.IPackageManager
 import android.os.Build
 import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
@@ -34,13 +35,26 @@ class XposedEntry : IXposedHookZygoteInit, IXposedHookLoadPackage {
             logI(TAG, "Hook entry")
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                findMethod("com.android.server.pm.BroadcastHelper") {
-                    name == "sendPackageBroadcastAndNotify"
-                }.hookBefore { param ->
-                    HMAService.instance?.handlePackageEvent(
-                        param.args[0] as String?,
-                        param.args[1] as String?
-                    )
+                try {
+                    findMethod("com.android.server.pm.BroadcastHelper") {
+                        name == "sendPackageBroadcastAndNotify"
+                    }.hookBefore { param ->
+                        HMAService.instance?.handlePackageEvent(
+                            param.args[0] as String?,
+                            param.args[1] as String?
+                        )
+                    }
+                } catch (e: Throwable) {
+                    findMethod("com.android.internal.content.PackageMonitor") {
+                        name == "onReceive"
+                    }.hookBefore { param ->
+                        val intent = param.args[1] as Intent? ?: return@hookBefore
+
+                        HMAService.instance?.handlePackageEvent(
+                            intent.action,
+                            intent.data?.encodedSchemeSpecificPart
+                        )
+                    }
                 }
             } else {
                 findMethod("com.android.server.pm.PackageManagerService") {
