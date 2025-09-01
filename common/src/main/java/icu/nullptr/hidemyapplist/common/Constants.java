67f8126bf451e4d14219e116dab37275fdde6dfa
frknkrc44 <krc440002@gmail.com>
2025-09-01 06:46:57 +0300

    Revert "Do not allow gms and gsf is added in any of lists"
    This reverts commit 71ea5103994206d8b2bfccd69999f1b3d918ab3c.

--- common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
+++ common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
     public static final String PROVIDER_AUTHORITY = APP_PACKAGE_NAME + ".ServiceProvider";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
+    public static final String UPDATE_URL_BASE = "https://api.nullptr.icu/android/hide-my-applist/static/";
+    public static final String TRANSLATE_URL = "https://crowdin.com/project/hide-my-applist";
 
     public static final String ANDROID_APP_DATA_ISOLATION_ENABLED_PROPERTY = "persist.zygote.app_data_isolation";
     public static final String ANDROID_VOLD_APP_DATA_ISOLATION_ENABLED_PROPERTY = "persist.sys.vold_app_data_isolation_enabled";
@@ -32,10 +34,6 @@ public class Constants {
             "com.android.providers.media.module",
             "com.android.providers.settings",
             "com.google.android.webview",
-            "com.google.android.providers.media.module",
-
-            // hiding apps from them causes crashes in hidden apps
-            GMS_PACKAGE_NAME,
-            GSF_PACKAGE_NAME
+            "com.google.android.providers.media.module"
     );
 }
